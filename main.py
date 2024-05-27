be3b3f24214fcc26782da478ba38780dcb21a033
AdiPat <contact.adityapatange@gmail.com>
2024-05-27 13:10:36 +0530

    add genai (gemini)

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import requests
 import traceback
 import os
 from dotenv import load_dotenv
+import google.generativeai as genai
 
 
 load_dotenv()
 
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+genai.configure(api_key=GEMINI_API_KEY)
 
 
 def fetch_html(url: str) -> str:
@@ -23,5 +25,15 @@ def fetch_reader_page(url: str) -> str:
         url = "https://r.jina.ai/" + url
         return fetch_html(url)
     except requests.exceptions.RequestException as e:
+        traceback.print_exc()
+        return None
+
+
+def ai_generate(prompt: str) -> str:
+    try:
+        model = genai.GenerativeModel("gemini-1.5-flash")
+        response = model.generate_content(prompt)
+        return response.text
+    except Exception as e:
         traceback.print_exc()
         return None
