6b2c1af5763956363d549613340b4a13fd0b9981
AdiPat <contact.adityapatange@gmail.com>
2024-05-27 13:08:26 +0530

    add dotenv support

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import requests
 import traceback
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
 
 
 def fetch_html(url: str) -> str:
